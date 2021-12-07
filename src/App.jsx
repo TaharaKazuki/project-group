acd69559a2dbafb22a1a177cc52135c25c48e268
Tahara Kazuki <>
2021-12-07 20:19:34 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-function App() {
+import Header from './components/Header'
+
+const App = () => {
   return (
     <>
+      <Header />
       <div className="container">
         <h1>My App</h1>
       </div>

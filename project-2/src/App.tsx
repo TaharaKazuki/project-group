d490e76d1d127d65e4bdb3423dc90eb3f0d05976
Tahara Kazuki <ssp0727.lnc@gmail.com>
2021-12-15 20:53:09 +0900

    update

--- project-2/src/App.tsx
+++ project-2/src/App.tsx
@@ -4,14 +4,19 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 // component
 import Navbar from './components/Navbar'
 import Footer from './components/layout/Footer'
+// page
+import About from './pages/About'
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main>Content</main>
-
+        <main className="flex flex-col justify-between h-screen">
+          <Switch>
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </main>
         <Footer />
       </div>
     </Router>
